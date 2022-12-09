28f560c15ea01e4c4ac4f72f41be591ce657c6ad
MartinNATAF <nataf.martin@gmail.com>
2022-12-10 00:27:28 +0100

    suppr point_loc_ou_vis models Match

--- tournement/polls/models.py
+++ tournement/polls/models.py
@@ -38,27 +38,6 @@ class Equipe(models.Model):
     point = models.IntegerField(default=0)
     def get_matches(self):
         matches= Match.objects.all().filter(locaux=self.ID) | Match.objects.all().filter(visiteur=self.ID)
-        for results in matches :
-            if results.visiteur.ID == self.ID:
-                if results.score_visiteurs > results.score_locaux:
-                    results.point_locaux = 0
-                    results.point_visteur = 3
-                elif results.score_visiteurs == results.score_locaux:
-                    results.point_locaux = 1
-                    results.point_visteur = 1
-                else:
-                    results.point_locaux = 3
-                    results.point_visteur = 0
-            else:
-                if results.score_visiteurs < results.score_locaux:
-                    results.point_locaux = 3
-                    results.point_visteur = 0
-                elif results.score_visiteurs == results.score_locaux:
-                    results.point_locaux = 1
-                    results.point_visteur = 1
-                else:
-                    results.point_locaux = 0
-                    results.point_visteur = 3
         return matches
     def __str__(self):
         return self.nom
@@ -68,29 +47,17 @@ class Equipe(models.Model):
         for results in matches :
             if results.visiteur.ID == self.ID:
                 if results.score_visiteurs > results.score_locaux:
-                    results.point_locaux = 0
-                    results.point_visteur = 3
                     final += 3
                 elif results.score_visiteurs == results.score_locaux:
-                    results.point_locaux = 1
-                    results.point_visteur = 1
                     final += 1
                 else:
-                    results.point_locaux = 3
-                    results.point_visteur = 0
                     final += 0
             else:
                 if results.score_visiteurs < results.score_locaux:
-                    results.point_locaux = 3
-                    results.point_visteur = 0
                     final += 3
                 elif results.score_visiteurs == results.score_locaux:
-                    results.point_locaux = 1
-                    results.point_visteur = 1
                     final += 1
                 else:
-                    results.point_locaux = 0
-                    results.point_visteur = 3
                     final += 0
         self.point = final
         return final
@@ -102,7 +69,5 @@ class Match(models.Model):
     locaux = models.ForeignKey(Equipe, on_delete=models.CASCADE, related_name="locaux")
     score_visiteurs = models.IntegerField(default=0)
     score_locaux = models.IntegerField(default=0)
-    point_locaux = models.IntegerField(default=0)
-    point_visteur = models.IntegerField(default=0)
     
     
