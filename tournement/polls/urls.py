bbab62a3c94d0edb4f4a60a2bfb5568c6f65fb27
MartinNATAF <nataf.martin@gmail.com>
2022-12-11 19:36:20 +0100

    add form for matches not played

--- tournement/polls/urls.py
+++ tournement/polls/urls.py
@@ -6,5 +6,7 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:equipe>/results', views.results, name='results'),
-    path('<int:ligue>/', views.detail, name='detail')
+    path('<int:ligue>/', views.detail, name='detail'),
+    path('<int:ligue>/post', views.post, name='post'),
+    path('post/<int:match>/', views.match, name='match'),
 ]
--- tournement/polls/views.py
+++ tournement/polls/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
+from django.forms import ModelForm
+from .forms import MatchForm
 
 from .models import Ligue, Equipe, Match
 
@@ -11,6 +13,38 @@ def detail(request, ligue):
     ligue = get_object_or_404(Ligue, pk=ligue)
     return render(request, 'polls/detail.html', {'ligue': ligue})
 
+
 def results(request, equipe):
     equipe = get_object_or_404(Equipe, pk=equipe)
     return render(request, 'polls/results.html', {'equipe': equipe})
+
+def post(request, ligue):
+    ligue = get_object_or_404(Ligue, pk=ligue)
+    # if this is a POST request we need to process the form data
+    form = MatchForm()
+    context = {'form': form, 'ligue': ligue}
+    return render(request, 'polls/post.html', context)
+
+def match(request, match):
+    match = get_object_or_404(Match, pk=match)
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = MatchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            print('mouhaha')
+            post = form.save(commit=False)
+            post.id = match.id
+            post.ligue = match.ligue
+            post.visiteur = match.visiteur
+            post.locaux = match.locaux
+            post.date = match.date
+            post.save()
+            url = "/polls/"+str(match.ligue.ID)+"/post"
+            return HttpResponseRedirect(url)
+        else :
+            print('top')
+    # if a GET or any other method) we'll create a blank form
+    else:
+        print('tozeeeeeeee')
+        return HttpResponseRedirect('/polls')
